45c30dd20926fa2dfe404b22a8b8694a787d60c7
AppBels <app.bels@gmail.com>
2017-02-25 15:50:57 +0100

    Add TieAction.class.php
    Add WinAction.class.php

---
+++ Schema/Thing/Action/AchieveAction/TieAction.class.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Org\Schema\Thing\Action\AchieveAction;
+
+/**
+ * Class TieAction.
+ * The act of reaching a draw in a competitive activity.
+ * @author      AppBels <app.bels@gmail.com>
+ * @name        TieAction
+ * @namespace   Org\Schema\Thing\Action\AchieveAction
+ * @package     Org\Schema
+ * @see         https://schema.org/TieAction
+ * Date:        25/02/2017
+ */
+class TieAction extends \Org\Schema\Thing\Action\AchieveAction
+{
+
+    /**
+     * TieAction constructor.
+     * @access  public
+     * @see     \Org\Schema\Thing\Action\AchieveAction::__construct()
+     */
+    public function __construct ()
+    {
+        parent::__construct();
+    }
+
+
+    /**
+     * TieAction destructor.
+     * @access  public
+     * @see     \Org\Schema\Thing\Action\AchieveAction::__destruct()
+     */
+    public function __destruct ()
+    {
+        parent::__destruct();
+    }
+
+}
+
+?>
---
+++ Schema/Thing/Action/AchieveAction/WinAction.class.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Org\Schema\Thing\Action\AchieveAction;
+
+/**
+ * Class WinAction.
+ * The act of achieving victory in a competitive activity.
+ * @author      AppBels <app.bels@gmail.com>
+ * @name        WinAction
+ * @namespace   Org\Schema\Thing\Action\AchieveAction
+ * @package     Org\Schema
+ * @see         https://schema.org/WinAction
+ * Date:        25/02/2017
+ */
+class WinAction extends \Org\Schema\Thing\Action\AchieveAction
+{
+
+    /**
+     * A sub property of participant. The loser of the action.
+     * @access  public
+     * @var     \Org\Schema\Thing\Person
+     */
+    public $loser;
+
+    /**
+     * WinAction constructor.
+     * @access  public
+     * @see     \Org\Schema\Thing\Action\AchieveAction::__construct()
+     */
+    public function __construct ()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * WinAction destructor.
+     * @access  public
+     * @see     \Org\Schema\Thing\Action\AchieveAction::__destruct()
+     */
+    public function __destruct ()
+    {
+        parent::__destruct();
+    }
+
+}
+
+?>
