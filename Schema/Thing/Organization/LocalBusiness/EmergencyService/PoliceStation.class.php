473a10e1e02f47978dcfd2a864988437e95deb00
appbels <app.bels@gmail.com>
2017-04-03 23:43:54 +0200

    Add PoliceStation.class.php

---
+++ Schema/Thing/Organization/LocalBusiness/EmergencyService/PoliceStation.class.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Org\Schema\Thing\Organization\LocalBusiness\EmergencyService;
+
+/**
+ * Class PoliceStation
+ * A police station.
+ * @author      AppBels <app.bels@gmail.com>
+ * @name        PoliceStation
+ * @namespace   Org\Schema\Thing\Organization\LocalBusiness\EmergencyService
+ * @package     Org\Schema
+ * @see         https://schema.org/PoliceStation
+ * Date         03/04/2017
+ */
+class PoliceStation extends \Org\Schema\Thing\Organization\LocalBusiness\EmergencyService
+{
+
+	/**
+	 * PoliceStation constructor.
+	 * @access  public
+	 * @see     \Org\Schema\Thing\Organization\LocalBusiness\EmergencyService::__construct()
+	 */
+	public function __construct ()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * PoliceStation toString.
+	 * @access  public
+	 * @see     \Org\Schema\Thing\Organization\LocalBusiness\EmergencyService::__toString()
+	 *
+	 * @return  string
+	 */
+	public function __toString ()
+	{
+		return parent::__toString();
+	}
+
+	/**
+	 * PoliceStation destructor.
+	 * @access  public
+	 * @see     \Org\Schema\Thing\Organization\LocalBusiness\EmergencyService::__destruct()
+	 */
+	public function __destruct ()
+	{
+		parent::__destruct();
+	}
+
+}
+
+?>
---
+++ Schema/Thing/Place/LocalBusiness/EmergencyService/PoliceStation.class.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Org\Schema\Thing\Place\LocalBusiness\EmergencyService;
+
+/**
+ * Class PoliceStation
+ * A police station.
+ * @author      AppBels <app.bels@gmail.com>
+ * @name        PoliceStation
+ * @namespace   Org\Schema\Thing\Place\LocalBusiness\EmergencyService
+ * @package     Org\Schema
+ * @see         https://schema.org/PoliceStation
+ * Date         03/04/2017
+ */
+class PoliceStation extends \Org\Schema\Thing\Place\LocalBusiness\EmergencyService
+{
+
+	/**
+	 * PoliceStation constructor.
+	 * @access  public
+	 * @see     \Org\Schema\Thing\Place\LocalBusiness\EmergencyService::__construct()
+	 */
+	public function __construct ()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * PoliceStation toString.
+	 * @access  public
+	 * @see     \Org\Schema\Thing\Place\LocalBusiness\EmergencyService::__toString()
+	 *
+	 * @return  string
+	 */
+	public function __toString ()
+	{
+		return parent::__toString();
+	}
+
+	/**
+	 * PoliceStation destructor.
+	 * @access  public
+	 * @see     \Org\Schema\Thing\Place\LocalBusiness\EmergencyService::__destruct()
+	 */
+	public function __destruct ()
+	{
+		parent::__destruct();
+	}
+
+}
+
+?>
